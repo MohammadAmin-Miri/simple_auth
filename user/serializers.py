6c4bae97b69477d805af7213caa42e2810793f1b
mohammad <mohammadaminmiri22@gmail.com>
2023-01-04 16:13:31 +0330

    Edited model field in CustomUserSerializer Meta class

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.core.cache import cache
 from rest_framework import serializers
 
 from user.models import CustomUser
 from user.tasks import send_verification_code
+
+user_model = get_user_model()
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -15,7 +19,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
+        user = user_model(**validated_data)
         user.set_password(password)
         user.save()
         send_verification_code.apply_async((validated_data.get('phone'), validated_data.get('email')))
